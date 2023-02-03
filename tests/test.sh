8c1e358e8916cc0840e2ad91fdef950ea4dd8811
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 21:03:00 +0300

    fixed tests status report

--- tests/test.sh
+++ tests/test.sh
@@ -7,4 +7,7 @@ rm -rf vendor
 rm -rf composer.lock
 composer install --dev
 rm -rf /tmp/VideoContentBundle
+
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
+status=$(cat $log | grep "ERRORS!")
+[ -z "$status" ] && exit 0 ||  exit -1
