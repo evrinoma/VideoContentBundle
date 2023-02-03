e7cd5526fc383f71f0b5c754a803cb3ff0c21d57
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:24:00 +0300

    fixed test clean up

--- tests/test.sh
+++ tests/test.sh
@@ -6,4 +6,5 @@ echo > $log
 rm -rf vendor
 rm -rf composer.lock
 composer install --dev
+rm -rf /tmp/VideoContentBundle
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
